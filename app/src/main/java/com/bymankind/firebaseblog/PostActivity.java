d1bce33c7621c3dbf22932b8a1380e85afba7d8d
akbarhakim29 <akhapr777@gmail.com>
2017-11-02 17:31:32 +0700

    Initial commit

---
+++ app/src/main/java/com/bymankind/firebaseblog/PostActivity.java
@@ -0,0 +1,118 @@
+package com.bymankind.firebaseblog;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class PostActivity extends AppCompatActivity {
+    private ImageButton mSelectImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+
+    private Button mSubmitBtn;
+
+    private Uri imageUri = null;
+    private static final int GALLERY_REQUEST = 1;
+
+    private StorageReference mStorage;
+    private DatabaseReference mDatabase;
+
+    private ProgressDialog mProgress ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+
+        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+        mPostTitle = (EditText) findViewById(R.id.titleField);
+        mPostDesc = (EditText) findViewById(R.id.descField);
+        mSubmitBtn = (Button) findViewById(R.id.submitBtn);
+
+        mProgress = new ProgressDialog(this);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+
+
+        final String title_val = mPostTitle.getText().toString().trim();
+        final String desc_val = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && imageUri != null){
+            mProgress.setMessage("Posting to Blog");
+            mProgress.show();
+            StorageReference filepath = mStorage.child("Blog_images").child(imageUri.getLastPathSegment());
+
+            filepath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                DatabaseReference newPost = mDatabase.push();
+
+                newPost.child("title").setValue(title_val);
+                newPost.child("description").setValue(desc_val);
+                newPost.child("image").setValue(downloadUrl.toString());
+                mProgress.dismiss();
+
+                startActivity(new Intent(PostActivity.this, MainActivity.class));
+                }
+            });
+        }else {
+            if (TextUtils.isEmpty(title_val)){
+                Toast.makeText(getApplication(), "Title is empty",Toast.LENGTH_SHORT).show();
+            }else if (TextUtils.isEmpty(desc_val)){
+                Toast.makeText(getApplication(), "Description is empty",Toast.LENGTH_SHORT).show();
+            }else if (imageUri == null){
+                Toast.makeText(getApplication(), "Image is empty",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            mSelectImage.setImageURI(imageUri);
+        }
+    }
+}
